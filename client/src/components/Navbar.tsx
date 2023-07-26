b0fa081a9c5694a28d03372f3a5f99f20d3d64c5
bdnrJ <jbednarczyk001@gmail.com>
2023-07-26 21:09:16 +0200

    client: a little work on the navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import logo from '../assets/logo.gif';
+import { Link } from 'react-router-dom';
+import {BiUser} from 'react-icons/bi'
+
+type links = {
+    name: string,
+    direction: string
+}
 
 const Navbar = () => {
+
+    const links: links[] = [
+        {
+            name: "Game",
+            direction: "/game"
+        },
+        {
+            name: "Rooms",
+            direction: "/rooms"
+        },
+        {
+            name: "Ranking",
+            direction: "/ranking"
+        },
+    ]
+
     return (
-        <div>Navbar</div>
+        <div className="navbar">
+            <div className="navbar--logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <div className="navbar--links">
+                {links.map((link) => (
+                    <Link to={link.direction}>
+                        <button>
+                            {link.name}
+                        </button>
+                    </Link>
+                ))}
+            </div>
+            <div className="navbar--account">
+                <button>
+                    <BiUser />
+                </button>
+            </div>
+        </div>
     )
 }
 
