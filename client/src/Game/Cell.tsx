1544b0ced89c6177f2af8713c3efcd474c438af2
bdnrJ <jbednarczyk001@gmail.com>
2023-08-03 12:43:58 +0200

    client: update game mechanics - ship placement

--- client/src/Game/Cell.tsx
+++ client/src/Game/Cell.tsx
@@ -1,4 +1,4 @@
-import React, { SetStateAction } from 'react'
+import React, { useState, SetStateAction } from 'react'
 
 type Props = {
     id: number
@@ -13,52 +13,88 @@ type Props = {
 
 const Cell = ({ id, board, rowId, columnId, setBoardState, isFlipped, setShipsCounter, shipsCounter }: Props) => {
 
-    const isPlacementPossible = (shipLength: string) => {
-        if (isFlipped) {
-            if ((rowId - parseInt(shipLength)) < -1) {
-                return false;
-            }
+    const isCellAvailable = (rowIdx: number, colIdx: number, shipLength: number, boardState: number[][], isFlipped: boolean) => {
+        // Check if the cell itself is available
+        if (boardState[rowIdx][colIdx] === 1) {
+            return false;
+        }
 
-            //check if trying to place a ship on already occupied place, or out of borders
-            for (let i = 0; i < parseInt(shipLength); i++) {
-                if (document.getElementById((id - (i * 10)).toString())?.classList.contains("--used"))
-                    return false;
+        // 10 x 10
+        const rows = boardState.length;
+        const cols = boardState[0].length;
+
+        const offsetsxd = [
+            [-1, -1], [-1, 0], [-1, 1],
+            [0, -1], [0, 1],
+            [1, -1], [1, 0], [1, 1],
+        ];
+
+        const offsets = [];
+
+        // set offsets based on the ship's length (horizontal or vertical placement)
+        // (no ship can be places next to other ship)
+        if (!isFlipped) {
+            for (let i = -1; i <= 1; i++) {
+                for (let j = -1; j <= shipLength; j++) {
+                    offsets.push([i, j])
+                }
             }
         } else {
-            //if ship tried to be placed is out of board borders
-            if ((columnId + parseInt(shipLength)) > 10) {
-                return false;
+            for (let i = (-shipLength); i <= 1; i++) {
+                for (let j = -1; j <= 1; j++) {
+                    offsets.push([i, j]);
+                }
             }
+        }
 
-            //check if trying to place a ship on already occupied place or out of borders
-            for (let i = 0; i < parseInt(shipLength); i++) {
-                if ((id + i) / 10 < Math.ceil(id / 10) || [0, 10, 20, 30, 40, 50, 60, 70, 80, 90].includes(id))
-                    if (document.getElementById((id + i).toString())?.classList.contains("--used"))
-                        return false;
+        console.log('-----');
+        
+        for (const [offsetRow, offsetCol] of offsets) {
+            const newRow = rowIdx + offsetRow;
+            const newCol = colIdx + offsetCol;
+
+            if(newCol > 10) return false;
+
+            if(isFlipped && newRow < -1) return false;
+
+
+            console.log(`[${newRow}, ${newCol}]`);
+            
+
+            // Check if the cell is within the matrix boundaries
+            if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+                // Check if the cell is already occupied by a ship
+                if ([1,2,3,4].includes(boardState[newRow][newCol])) {
+                    return false;
+                }
             }
         }
 
         return true;
-    }
+    };
+
 
     const dropShip = (e: any) => {
         //the length of the ship [1 - 4] is stored as ship id
         const shipLength = document.querySelector('.beingDragged')?.id;
+
 
         if (!shipLength) {
             console.log("ship id is not defined (should not be possible)");
             return;
         }
 
-        const isPossible = isPlacementPossible(shipLength);
+        const shipLengthNumber = parseInt(shipLength)
+
+        const isPossible = isCellAvailable(rowId, columnId, shipLengthNumber, board, isFlipped);
 
         if (isPossible) {
             if (isFlipped) {
                 const newBoard = board.map((row, rowIndex) =>
-                    rowIndex <= rowId && rowIndex > rowId - parseInt(shipLength)
+                    rowIndex <= rowId && rowIndex > rowId - shipLengthNumber
                         ? row.map((cell, cellIndex) => {
                             if (cellIndex === columnId) {
-                                return 1; // Update the cell at the specified column to 1
+                                return shipLengthNumber; // Update the cell at the specified column to 1
                             } else {
                                 return cell; // Keep other cells unchanged
                             }
@@ -70,8 +106,8 @@ const Cell = ({ id, board, rowId, columnId, setBoardState, isFlipped, setShipsCo
                 const newBoard = board.map((row, rowIndex) =>
                     rowIndex === rowId
                         ? row.map((cell, cellIndex) => {
-                            if (cellIndex >= columnId && cellIndex < columnId + parseInt(shipLength)) {
-                                return 1; // Update the cells within the ship's length to 1
+                            if (cellIndex >= columnId && cellIndex < columnId + shipLengthNumber) {
+                                return shipLengthNumber; // Update the cells within the ship's length to 1
                             } else {
                                 return cell; // Keep other cells unchanged
                             }
@@ -97,7 +133,7 @@ const Cell = ({ id, board, rowId, columnId, setBoardState, isFlipped, setShipsCo
             return;
         }
 
-        let isPossible = isPlacementPossible(shipLength);
+        const isPossible = isCellAvailable(rowId, columnId, parseInt(shipLength), board, isFlipped);
 
         addAdditionalClasses(isPossible, shipLength);
     }
