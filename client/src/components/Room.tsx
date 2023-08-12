f889b34b292a176ce8489a4c4bb3984e892e02b3
bdnrJ <jbednarczyk001@gmail.com>
2023-08-12 12:14:42 +0200

    client: room and rooms visual update

--- client/src/components/Room.tsx
+++ client/src/components/Room.tsx
@@ -1,32 +1,38 @@
-import { useContext } from 'react'
-import { GameRoomType } from '../context/RoomContext'
-import socket from '../utils/socket'
-import { UserContext } from '../context/UserContext'
+import { useContext } from "react";
+import { GameRoomType } from "../context/RoomContext";
+import socket from "../utils/socket";
+import { UserContext } from "../context/UserContext";
+import {AiOutlineArrowRight} from 'react-icons/ai'
 
 type Props = {
-    gameRoom: GameRoomType
-}
+	gameRoom: GameRoomType;
+};
 
 const Room = ({ gameRoom }: Props) => {
+	const { user } = useContext(UserContext);
+	if (gameRoom.clients.length === 0 || gameRoom.clients.length === 2) return null;
 
-    const { user } = useContext(UserContext);
-    if (gameRoom.clients.length === 0 || gameRoom.clients.length === 2) return null;
+	const onRoomJoin = () => {
+		socket.emit("joinRoom", gameRoom.id, user.nickname);
+	};
 
-    const onRoomJoin = () => {
-        socket.emit('joinRoom', gameRoom.id, user.nickname);
-    }
+	return (
+		<div className='room' key={gameRoom.hostName + gameRoom.roomName}>
+			<div className='room__elem'>
+				<span className='room__elem--title'>Host</span> <span>{gameRoom.hostName}</span>
+			</div>
+			<div className='room__elem'>
+				<span className='room__elem--title'>Name</span> <span>{gameRoom.roomName}</span>
+			</div>
+			<div className='room__elem'>
+				<span className='room__elem--title'>Players</span> <span>{gameRoom.clients.length}/2</span>
+			</div>
+			<button className='room__button' onClick={onRoomJoin}>
+				<span>Join Room</span>
+				<AiOutlineArrowRight />
+			</button>
+		</div>
+	);
+};
 
-    return (
-        <div className="room" key={gameRoom.hostName + gameRoom.roomName}>
-            <span>Host: {gameRoom.hostName}</span>
-            <span>Name: {gameRoom.roomName}</span>
-            <span>Password: {gameRoom.hasPassword}</span>
-            <span>Password xd lmao: {gameRoom.password}</span>
-            <span>id: {gameRoom.id}</span>
-            <span>players: {gameRoom.clients.length}/2</span>
-            <button onClick={onRoomJoin} >Join Room</button>
-        </div>
-    )
-}
-
-export default Room
+export default Room;
--- client/src/views/Rooms.tsx
+++ client/src/views/Rooms.tsx
@@ -1,69 +1,74 @@
-import { useContext, useEffect, useState, useRef } from 'react';
-import socket from '../utils/socket';
-import Modal from '../components/modals/Modal';
-import CreateRoom from '../components/modals/CreateRoom';
-import Room from '../components/Room';
-import { v4 as uuidv4 } from 'uuid';
-import { useNavigate } from 'react-router-dom';
-import { GameRoomType, RoomContext } from '../context/RoomContext';
-import { UserContext } from '../context/UserContext';
-
-
+import { useContext, useEffect, useState, useRef } from "react";
+import socket from "../utils/socket";
+import Modal from "../components/modals/Modal";
+import CreateRoom from "../components/modals/CreateRoom";
+import Room from "../components/Room";
+import { useNavigate } from "react-router-dom";
+import { GameRoomType, RoomContext } from "../context/RoomContext";
+import { UserContext } from "../context/UserContext";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const Rooms = () => {
-  const [isCreateRoomVisible, setIsCreateRoomVisible] = useState<boolean>(false);
-  const [rooms, setRooms] = useState<GameRoomType[]>([]);
-  const {setRoom} = useContext(RoomContext);
-  const {user} = useContext(UserContext);
-  const navigate = useNavigate();
+	const [isCreateRoomVisible, setIsCreateRoomVisible] = useState<boolean>(false);
+	const [rooms, setRooms] = useState<GameRoomType[]>([]);
+	const { setRoom } = useContext(RoomContext);
+	const { user } = useContext(UserContext);
+	const navigate = useNavigate();
 
-  //to make useeffect work once for tests :)
-  const effectCalled = useRef<boolean>(false);
+	//to make useeffect work once for tests :)
+	const effectCalled = useRef<boolean>(false);
 
-  useEffect(() => {
-    // if (effectCalled.current) return;
-    // Emit the event to request the list of available rooms
-    socket.emit('getRooms');
+	useEffect(() => {
+		// if (effectCalled.current) return;
+		// Emit the event to request the list of available rooms
+		socket.emit("getRooms");
 
-    // Listen for the event that sends the list of available rooms
-    socket.on('roomsList', (availableRooms) => {
-      setRooms([...availableRooms]);
-    });
+		// Listen for the event that sends the list of available rooms
+		socket.on("roomsList", (availableRooms) => {
+			setRooms([...availableRooms]);
+		});
 
-    socket.on('roomJoined', ((room) => {
-      setRoom(room);
-      navigate(`/room/${room.id}`)
-    }))
+		socket.on("roomJoined", (room) => {
+			setRoom(room);
+			navigate(`/room/${room.id}`);
+		});
 
-    socket.on('roomError', (error) => console.log(error))
+		socket.on("roomError", (error) => console.log(error));
 
-    // Clean up the event listener when the component unmounts
-    effectCalled.current = true;
-    return () => {
-      socket.off('roomsList'); // Remove the 'roomsList' event listener
-      socket.off('roomJoined'); // Remove the 'roomJoined' event listener
-      socket.off('roomError'); // Remove the 'roomError' event listener
-    };
-  }, []); // Empty dependency array to run the effect only once when the component mounts
+		// Clean up the event listener when the component unmounts
+		effectCalled.current = true;
+		return () => {
+			socket.off("roomsList"); // Remove the 'roomsList' event listener
+			socket.off("roomJoined"); // Remove the 'roomJoined' event listener
+			socket.off("roomError"); // Remove the 'roomError' event listener
+		};
+	}, []); // Empty dependency array to run the effect only once when the component mounts
 
-  // Function to create a new room
-  const createRoom = (gameRoom: GameRoomType) => {
-    socket.emit('createRoom', gameRoom, user.nickname); // You can change the room name as needed
-  };
+	// Function to create a new room
+	const createRoom = (gameRoom: GameRoomType) => {
+		socket.emit("createRoom", gameRoom, user.nickname); // You can change the room name as needed
+	};
 
-  return (
-    <div className="rooms">
-      {rooms.map((room) => (
-        <Room key={room.id} gameRoom={room} />
-      ))}
-      <button onClick={() => setIsCreateRoomVisible(true)}>Create Room</button>
-      {isCreateRoomVisible &&
-        <Modal onClose={() => setIsCreateRoomVisible(false)}>
-          <CreateRoom closePopup={() => setIsCreateRoomVisible(false)} createRoom={createRoom}/>
-        </Modal>
-        }
-    </div>
-  );
+	return (
+		<div className='rooms'>
+			<div className='rooms__controlls'>
+				<button onClick={() => setIsCreateRoomVisible(true)}>
+					<span>Create Room</span>
+					<AiOutlinePlus />
+				</button>
+			</div>
+			<div className='rooms__game-rooms'>
+				{rooms.map((room) => (
+					<Room key={room.id} gameRoom={room} />
+				))}
+			</div>
+			{isCreateRoomVisible && (
+				<Modal onClose={() => setIsCreateRoomVisible(false)}>
+					<CreateRoom closePopup={() => setIsCreateRoomVisible(false)} createRoom={createRoom} />
+				</Modal>
+			)}
+		</div>
+	);
 };
 
 export default Rooms;
